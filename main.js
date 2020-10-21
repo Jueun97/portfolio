0ac2a1601b51cc0e80b4eca712efcb53dc07c6dc
Jueun <zxnm0901@naver.com>
2020-10-22 01:02:43 +0900

    javascript. handling cliked menu
    move to clicked section view

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 //js 사용 시 필수적으로 선언해줘야 함! 우리의 미친 짓을 사전의 방지 가능
 'use strict'
+
+// change navbar color according to its height
 window.addEventListener('scroll', e => {
     //e.path[1].scrollY =  window.scrollY
     const scrollY = window.scrollY;
     const navbar = document.querySelector('#navbar');
     const navbarHeight = navbar.offsetHeight;
-    console.log(navbarHeight);
 
     if (scrollY > navbarHeight)
         navbar.classList.add('navbar--dark');
@@ -14,3 +15,26 @@ window.addEventListener('scroll', e => {
     
     
 })
+
+// move to clicked section
+const navbarMenu = document.querySelector('.navbar__menu'); 
+navbarMenu.addEventListener('click', (event => {
+    const target = event.target;
+    const value = target.dataset.value; 
+    if (value == null)
+        return;
+   
+    const section = document.querySelector(value);
+    section.scrollIntoView({ behavior: "smooth", block: "center" });
+    // highligt the clicked section button
+    const item = navbarMenu.children;
+    for (let i = 0; i < item.length; i++){
+        if (item[i].dataset.value != value)
+            item[i].classList.remove('active');
+        else
+            item[i].classList.add('active');
+    }
+
+}))
+
+
